3a7a4368d03d15ca908f8b9cfc55f6c4bc701a56
Ruslan Makhmutov <ru.makhmutov@danaflex.ru>
2024-02-02 02:11:03 +0300

    typescript

--- src/arrays-objects-methods.js
+++ src/arrays-objects-methods.js
@@ -1,27 +1,24 @@
+"use strict";
+console.log('--------------------------');
+console.log('arrays-objects-methods.js');
 // 1
-const prices = [100, 150, 200, 250]
-const pricesWithCurrency = prices.map((price) => `${price} р`)
-console.log('массив цен со знаком рубля:', pricesWithCurrency)
-console.log()
-
-// 2
+const prices = [100, 150, 200, 250];
+const pricesWithCurrency = prices.map((price) => `${price} р`);
+console.log('массив цен со знаком рубля:', pricesWithCurrency);
+console.log();
 const users = [
     ['alex', 32],
     ['tomas', 17],
     ['olga', 14],
     ['andre', 24]
-]
-
-const usersAdult = users.filter((user) => user[1] >= 18)
-console.log('массив совершеннолетних пользователей:', usersAdult)
-console.log()
-
-// 3
+];
+const usersAdult = users.filter((user) => Number(user[1]) >= 18);
+console.log('массив совершеннолетних пользователей:', usersAdult);
+console.log();
 const goods = [
     { title: 'пицца', price: 200 },
     { title: 'баранина', price: 300 },
     { title: 'креветки', price: 400 }
-]
-
-const sumGoodsPrices = goods.reduce((acc, item) => (acc += item.price), 0)
-console.log('общая стоимость товаров:', sumGoodsPrices)
+];
+const sumGoodsPrices = goods.reduce((acc, item) => (acc += item.price), 0);
+console.log('общая стоимость товаров:', sumGoodsPrices);
--- src/async.ts
+++ src/async.ts
@@ -1,35 +1,57 @@
-"use strict";
+interface PlaceholderComment {
+    postId: number
+    id: number
+    name: string
+    email: string
+    body: string
+}
+
+interface PlaceholderPhoto {
+    albumId: number
+    id: number
+    title: string
+    url: string
+    thumbnailUrl: string
+}
+
+interface PlaceholderAlbum {
+    userId: number
+    id: number
+    title: string
+}
+
 // параллельные запросы, по завершении всех выводятся в правильном порядке
 Promise.all([
     fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
         .then((res) => res.json())
-        .then((data) => {
-        // console.log(1)
-        return data.filter((item) => item.id % 2 === 0);
-    }),
+        .then((data: PlaceholderComment[]) => {
+            // console.log(1)
+            return data.filter((item: PlaceholderComment): boolean => item.id % 2 === 0)
+        }),
     fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
         .then((res) => res.json())
-        .then((data) => {
-        // console.log(2)
-        return data.filter((item) => item.title[0] === 'a');
-    }),
+        .then((data: PlaceholderPhoto[]) => {
+            // console.log(2)
+            return data.filter((item: PlaceholderPhoto): boolean => item.title[0] === 'a')
+        }),
     fetch('https://jsonplaceholder.typicode.com/users/1/albums')
         .then((res) => res.json())
-        .then((data) => {
-        // console.log(3)
-        return data.filter((item) => item.id > 5);
-    })
+        .then((data: PlaceholderAlbum[]) => {
+            // console.log(3)
+            return data.filter((item: PlaceholderAlbum): boolean => item.id > 5)
+        })
 ])
-    .then((data) => {
-    console.log('--------------------------');
-    console.log('async.js');
-    console.log('комментарии с четным id:', data[0]);
-    console.log('фотографии с буквой "a" в начале title:', data[1]);
-    console.log('альбомы с id > 5:', data[2]);
-})
-    .catch((e) => {
-    console.log(e);
-});
+    .then((data: [PlaceholderComment[], PlaceholderPhoto[], PlaceholderAlbum[]]): void => {
+        console.log('--------------------------')
+        console.log('async.js')
+        console.log('комментарии с четным id:', data[0])
+        console.log('фотографии с буквой "a" в начале title:', data[1])
+        console.log('альбомы с id > 5:', data[2])
+    })
+    .catch((e): void => {
+        console.log(e)
+    })
+
 // // параллельные никак не связанные запросы, порядок вывода в консоли зависит от скорости ответа от сервера
 // fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
 //     .then((response) => response.json())
@@ -51,6 +73,7 @@ Promise.all([
 //         const albumsHaveIdMoreThan5 = data.filter((item) => item.id > 5)
 //         console.log('альбомы с id > 5:', albumsHaveIdMoreThan5)
 //     })
+
 // // последовательные запросы, по завершении всех выводятся в правильном порядке
 // const printPlaceholders = async () => {
 //     const data1 = await fetch(
